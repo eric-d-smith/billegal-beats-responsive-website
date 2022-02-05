cf0ad3c93cbaa1f03cc7601189301770fb1cdf1a
eric d smith <ericsmithdesigns@gmail.com>
2022-02-04 16:24:22 -0800

    Landing page for XL screens almost complete. Added bouncing scroll arrow at bottom.

--- js/scripts.js
+++ js/scripts.js
@@ -18,3 +18,11 @@ $(document).ready(function(){
     });
   });
 // END hamburger animation
+
+// Bouncing arrow bottom of full screen
+// Different arrow choices from Font Awesome
+// Fades away as you scroll down
+$(window).scroll(function(){
+  $(".arrow").css("opacity", 1 - $(window).scrollTop() / 250); 
+//250 is fade pixels
+});
